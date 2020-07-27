8b7c8428bb8f1b61f655a740b673b0f33096a28e
daBard <50872455+daBard@users.noreply.github.com>
2020-07-27 08:51:54 +0200

    Resize working, need to fix confirmation if

--- myscript.js
+++ myscript.js
@@ -8,6 +8,7 @@ const docMenu = document.querySelector('#menu');
 const docSaveBtn = document.querySelector('#save-btn');
 const docClearBtn = document.querySelector('#clear-btn');
 const docSizeBtn = document.querySelector('#size-btn');
+const docSizeBtnText = document.querySelector('#size-txt');
 const docSizeBox = document.querySelector('#set-size');
 const docSizeX = document.querySelector('#size-x');
 const docSizeY = document.querySelector('#size-y');
@@ -56,15 +57,29 @@ docSizeBox.addEventListener('click', function(e){
 });
 
 docResizeY.addEventListener('click', function() {
-    canvasX = docSizeX.value;
-    canvasY = docSizeY.value;
-    console.log(canvasX + ', ' + canvasY);
-    //wipe all pixels
-    //set css grid (maybe inside initCanvas?)
-    initCanvas();
-    setCanvasSize();
-    docSizeBox.classList.toggle('blocker');
-    docSizeBox.classList.toggle('hidden');
+        let pixels = document.querySelectorAll('.pixel');
+        let sugCanvasX = parseInt(docSizeX.value);
+        let sugCanvasY = parseInt(docSizeY.value);
+
+        if ((sugCanvasX > 0 && sugCanvasX <= 64) && (sugCanvasY > 0 && sugCanvasY <= 64)) {
+            canvasX = sugCanvasX;
+            canvasY = sugCanvasY;
+        }
+        else {
+            alert('Please pick a size between 1-64 pixels!');
+        }
+    
+        for (i=0; i < pixels.length; i++) {
+            pixels[i].parentNode.removeChild(pixels[i]);
+        }
+
+        initCanvas();
+        setCanvasSize();
+        docSizeBox.classList.toggle('blocker');
+        docSizeBox.classList.toggle('hidden');
+
+        //THIS IS CRAP AT THE MOMENT, FIX IT STUPID!
+        //if(confirm('WARNING!\nResizing the canvas will erase your current work!')) {
 });
 
 // INITIALIZE PALETTE
@@ -96,14 +111,15 @@ for (i=0; i < docColDiv.length; i++) {
 
 // INITIALIZE PAINTABLE CANVAS
 function initCanvas() {
+    docCanvas.style.gridTemplateColumns = `repeat(${ canvasX }, 1fr)`;
     for (i=0; i < canvasY; i++) {
         for (j=0; j < canvasX; j++) {
-            let docDiv = document.createElement('div');
-            docDiv.classList.add('pixel');
+            let docPixel = document.createElement('div');
+            docPixel.classList.add('pixel');
 
             let currentPixelColor;
 
-            docDiv.addEventListener('mouseover', function() {
+            docPixel.addEventListener('mouseover', function() {
                 if (isDrawing == true) {
                     this.style.cssText = `background-color: ${ drawCol }; ${ pixelSize }`;
                     currentPixelColor = drawCol;
@@ -113,45 +129,47 @@ function initCanvas() {
                 }   
             });
 
-            docDiv.addEventListener('mouseout', function() {
+            docPixel.addEventListener('mouseout', function() {
                 this.style.cssText = `background-color: ${ currentPixelColor }; ${ pixelSize }`;
             });
 
-            docDiv.addEventListener('mousedown', function(e) {
+            docPixel.addEventListener('mousedown', function(e) {
                 e.preventDefault();
                 this.style.cssText = `background-color: ${ drawCol }; ${ pixelSize }`;
                 currentPixelColor = drawCol;
                 isDrawing = true;
             });
 
-            docDiv.addEventListener('mouseup', function() {
+            docPixel.addEventListener('mouseup', function() {
                 isDrawing = false;
             });
 
-            docCanvas.appendChild(docDiv);
+            docCanvas.appendChild(docPixel);
         }
     }
+    docSizeBtnText.innerHTML = `Size:<br>${ canvasX }x${ canvasY }`;
 }
 
 // CALCULATE CANVAS AND PIXEL SIZE
 function setCanvasSize() {
+    let pixels = document.querySelectorAll('.pixel');
     let maxSize = Math.min(docMain.clientHeight, docMain.clientWidth);
     let size;
     let bgCol;
 
     if ( canvasX > canvasY ) {
-        size = maxSize / (canvasY + 2);
+        size = maxSize / (canvasX + 2);
     }
     else {
-        size = maxSize / (canvasX + 2);
+        size = maxSize / (canvasY + 2);
     }
 
     size = Math.floor(size);
 
     pixelSize = `height: ${ size }px; width: ${ size }px;`;
     
-    pixels = document.querySelectorAll('.pixel');
     
+
     for (i=0; i < pixels.length; i++) {
         bgCol = window.getComputedStyle(pixels[i]).getPropertyValue('background-color');
         pixels[i].style.cssText = `background-color: ${ bgCol }; ${ pixelSize }`;
@@ -159,7 +177,6 @@ function setCanvasSize() {
 
 }
 
-// editable size https://www.w3schools.com/cssref/tryit.asp?filename=trycss_js_grid-template-columns
 // grid overlay https://onagova.github.io/etch-a-sketch/
 // clear button
 // save button (save div as image)???
